7aaf2fc1dc58dbcf8df17d5ad2c3e8bacd05b225
shota nakaguchi <hare.tokidoki.kumori227@gmail.com>
2019-02-02 16:58:28 +0900

    signup

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,12 @@ Rails.application.routes.draw do
 
   get '/signup' =>'users#new'
   root 'static_pages#home'
+  post '/signup',  to: 'users#create'
 
   get '/help' =>'static_pages#help'
   get '/about' => 'static_pages#about'
   get  '/contact' => 'static_pages#contact'
+   resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
